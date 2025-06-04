92bc360d70438c295d2ddf576b8de58d5192b46c
Kaio991 <kaioalencar.santos@gmail.com>
2025-06-04 20:22:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  document.getElementById('mensagem').innerText =
+    "Meu amor, eu não tenho palavras pra descrever o quanto eu amo você, eu não sabia o quanto é incrível a vida antes de te conhecer. Eu sou tão feliz com você, minha sereia. Você me completa, é o amor da minha vida. Quero viver o resto da minha vida ao seu lado. Eu sei que não sou perfeito, mas vou sempre tentar melhorar pra você. Eu te amo infinitamente. Sempre vou tentar te fazer sorrir, minha princesa.";
+}
